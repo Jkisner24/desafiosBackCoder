d082d9feac8b2dbc05ba92aaddb95fed582f95bc
Julian Kisner <julikisner@gmail.com>
2023-04-27 12:20:13 -0300

    reordenamiento

---
+++ Desafios/src/handlers/viewHandlers.js
@@ -0,0 +1,33 @@
+const ProductManager = require("../controllers/ProductManager")
+
+const producto = new ProductManager("../products.json")
+
+const viewProductStatic = async(req, res) =>{
+    try{
+        const prodList =  await producto.getProducts()
+        
+        let datosProd = {
+            listProducts: prodList,
+            style: 'index.css'
+        }
+        res.render('home', datosProd)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+const viewProductLive =  async(req, res) =>{
+    try{
+        const prodList =  await producto.getProducts()
+        
+        let datosProd = {
+            listProdLive: prodList
+        }
+        res.render('realtimeprod', datosProd)
+    }catch(error){
+        console.log(error)
+    }
+}    
+
+module.exports = {viewProductStatic, viewProductLive}
+
